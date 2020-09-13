5353d4cd258bc7e6202b3f72c346b935c6797336
bellm00n <bellinthegame@gmail.com>
2020-09-13 17:28:28 +0300

    Add EventObserver

---
+++ src/events/index.ts
@@ -0,0 +1,5 @@
+import EventObserver from '../utils/EventObserver';
+
+export const tickEvent = new EventObserver();
+export const spinButtonClickEvent = new EventObserver();
+export const spinStartEvent = new EventObserver();
---
+++ src/utils/EventObserver.ts
@@ -0,0 +1,17 @@
+type observerType = (date: object) => void;
+
+export default class EventObserver {
+    private observers: Array<observerType> = [];
+
+    subscribe(fn: observerType) {
+      this.observers.push(fn);
+    }
+
+    unsubscribe(fn: observerType) {
+      this.observers = this.observers.filter((subscriber : observerType) => subscriber !== fn);
+    }
+
+    broadcast(data: object) {
+      this.observers.forEach((subscriber: observerType) => subscriber(data));
+    }
+}
